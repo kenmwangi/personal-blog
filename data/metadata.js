9837a55d41eacb61be1acdacd04b1b02232f1e6e
Ken Mwangi <kenmwangi071@gmail.com>
2022-10-17 13:31:39 +0300

    sitemeta data

---
+++ data/metadata.js
@@ -0,0 +1,20 @@
+const metadata = {
+  author: "Ken Mwangi",
+  title: "Home",
+  headerTitle: "Ken Mwangi",
+  description:
+    "Jambo, I'm Ken Mwangi. Fullstack Developer who makes world a better place through quality software, open-source and technical writing",
+  language: "en-us",
+  locale: "en-US",
+  siteUrl: "http://personal-blog-nu-taupe.vercel.app/",
+  siteRepo: "https://github.com/kenmwangi/personal-blog",
+  email: "kenmwangi071@gmail.com",
+  twitter: "https://twitter.com/ken_cipher",
+  github: "https://github.com/kenmwangi",
+  linkedin: "https://www.linkedin.com/in/kkmwangi/",
+  facebook: "https://www.facebook.com/ken.mwangi.96742",
+
+  //   add analytics later HERE!
+};
+
+module.exports = metadata;
